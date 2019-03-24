0f4ed632cf1a4eb667ab873374d22bd0845ce271
miriamCR <miriamcabana96@gmail.com>
2019-03-24 16:39:25 +0100

    Haciendo pmx, modificado mut.inversion

--- metodosCruce/CrucePMX.java
+++ metodosCruce/CrucePMX.java
@@ -27,21 +27,24 @@ public class CrucePMX implements AlgoritmoCruce {
 		for (int i = 0; i < nSelCruce; i+=2) {
 			hijo1 = new Cromosoma();
 			hijo2 = new Cromosoma();
-			
+
 			crucePMX(pob[selCruce[i]], pob[selCruce[i+1]], hijo1, hijo2);
 
 			// se evaluan
 			hijo1.setFitness(hijo1.evaluaCromosoma());
 			hijo2.setFitness(hijo2.evaluaCromosoma());
-			
+
 			// los nuevos individuos sustituyen a sus progenitores
 			pob[selCruce[i]] = hijo1;
 			pob[selCruce[i+1]] = hijo2;
 		}
 	}
-	
+
 
 	private void crucePMX(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2) {
-		
+			int nGenes = padre1.getnGenes();
+			int pos1 = (int) (Math.random() * (nGenes - 1)); //0 incluido y ngenes-1 no(ultima pos no incluida)
+			int pos2 = (int) (Math.random() * (nGenes) + (pos1 + 1)); //pos1+1 incluido y ngenes no (ultima pos incluida)
+
 	}
 }
