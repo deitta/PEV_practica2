7136065a1d0f873fd10aafd6ce9356d1ed934f9f
miriamCR <miriamcabana96@gmail.com>
2019-03-25 18:45:01 +0100

    cambios en mut.inversion y agregado crucepmx

--- metodosMutacion/MutacionInversion.java
+++ metodosMutacion/MutacionInversion.java
@@ -8,20 +8,23 @@ public class MutacionInversion implements AlgoritmoMutacion {
 	public void mutacion(Cromosoma[] pob, double probMutacion, int tamPob) {
 		boolean mutado;
 		double prob;
-		int pos1, pos2, aux;
+		int puntDC1, puntDC2, puntDCAux, aux;
 		int nGenes = pob[0].getnGenes();
 
 		for (int i = 0; i < tamPob; i++) {
 			mutado = false;
 			prob = Math.random();	// se genera un numero aleatorio entre [0 1]
 			if (prob < probMutacion){	// mutan los genes con prob<probMutacion
-				pos1 = (int) (Math.random() * (nGenes - 1)); //0 incluido y ngenes-1 no(ultima pos no incluida)
-				pos2 = (int) (Math.random() * (nGenes) + (pos1 + 1)); //pos1+1 incluido y ngenes no (ultima pos incluida)
-				for (int j = pos1; j <= pos2/2; j++) {
-					aux = pob[i].genes[pos2];
-					pob[i].genes[pos2] = pob[i].genes[j];
+				puntDC1 = (int) (Math.random()*nGenes), puntDC2 = (int) (Math.random()*nGenes), puntDCAux = puntDC1;
+				if (puntDC1 > puntDC2){
+					puntDC1 = puntDC2;
+					puntDC2 = puntDCAux;
+				}
+				for (int j = puntDC1; j <= puntDC2/2; j++) {
+					aux = pob[i].genes[puntDC2];
+					pob[i].genes[puntDC2] = pob[i].genes[j];
 					pob[i].genes[j] = aux;
-					pos2--;
+					puntDC2--;
 				}
 			mutado = true;
 			}
