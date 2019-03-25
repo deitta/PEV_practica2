284627bc1d6afa1ec057fc200eff98c63e5d2ed6
miriamCR <miriamcabana96@gmail.com>
2019-03-25 03:58:54 +0100

    crucepmx ok!

--- metodosCruce/CrucePMX.java
+++ metodosCruce/CrucePMX.java
@@ -3,49 +3,42 @@ package metodosCruce;
 import base.Cromosoma;
 
 public class CrucePMX implements AlgoritmoCruce {
-	public void cruce(Cromosoma[] pob, int tamPob, double probCruce) {
+	public void cruce(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2) {
 		int nGenes = padre1.getnGenes();
-		int puntDC1 = (int) (Math.random() * (nGenes - 1)); //0 incluido y ngenes-1 no(ultima pos no incluida)
-		int puntDC2 = (int) (Math.random() * (nGenes) + (pos1 + 1)); //pos1+1 incluido y ngenes no (ultima pos incluida)
+		int puntDC1 = (int) (Math.random()*nGenes), puntDC2 = (int) (Math.random()*nGenes), puntDCAux = puntDC1;
+		if (puntDC1 > puntDC2){
+			puntDC1 = puntDC2;
+			puntDC2 = puntDCAux;
+		}
 
 		for (int i = puntDC1; i <= puntDC2; i++){
 			hijo1.genes[i].setCiudad(padre2.genes[i].getCiudad());
 			hijo2.genes[i].setCiudad(padre1.genes[i].getCiudad());
 		}
 
-		boolean conflicto = false;
-		// indices de las posiciones donde no hay conflicto para hijo1 e hijo2 repectivamente
-		int indP1 = puntDC2 + 1, indP2 = puntDC2 + 1, j;
-		// rellena las posiciones del hijo en orden puntDC2 -> nGenes y de 0 -> puntDC1
 		for (int i = puntDC2 + 1; i >= puntDC2 || i < puntDC1; i = (i+1) % nGenes) {
-			do {
-				j = puntDC1; //para recorrer el array entre los puntDC
-				do{
-					conflicto = hijo1.genes[j].getCiudad() == padre1.genes[indP1].getCiudad();
-					j = (j+1) % nGenes;
-				}while(!conflicto && j != i);
-				// si hay conflicto pasamos a la siguiente ciudad
-				if (conflicto) {
-					indP1 = (indP1+1) % nGenes;
-					i++;
-				}
-			} while (conflicto);
-			hijo1.genes[i].setCiudad(padre1.genes[indP1].getCiudad());
+			//para el hijo 1
+			int pos = funcionaux(padre1.genes[i].getCiudad(), puntDC1, puntDC2, hijo1);
+			if(pos == -1)
+				hijo1.genes[i].setCiudad(padre1.genes[i].getCiudad());
+			else
+				hijo1.genes[i].setCiudad(padre1.genes[pos].getCiudad());
+			//para el hijo 2
+			pos = funcionaux(padre2.genes[i].getCiudad(), puntDC1, puntDC2, hijo2);
+			if(pos == -1)
+				hijo2.genes[i].setCiudad(padre2.genes[i].getCiudad());
+			else
+				hijo2.genes[i].setCiudad(padre2.genes[pos].getCiudad());
 
-			// lo mismo de antes pero para el hijo2
-			do {
-				j = puntDC1;
-				do {
-					conflicto = hijo2.genes[j].getCiudad() == padre2.genes[indP2].getCiudad();
-					j = (j+1) % nGenes;
-				} while (!conflicto && j != i);
-				if (conflicto) {
-					indH2 = (indH2+1) % nGenes;
-					i++;
-				}
-			} while (conflicto);
-			hijo2.genes[i].setCiudad(padre2.genes[indP2].getCiudad());
+			//	hijo2.genes[i].setCiudad(padre2.genes[pos==-1?i:pos].getCiudad());
 		}
+	}
 
+
+	private int funcionaux(int num, int puntDC1, int puntDC2, Cromosoma cromosoma){
+		while(num != cromosoma.genes[puntDC1].getCiudad && puntDC1 <= puntDC2)
+			puntDC1++;
+		if (puntDC1 > puntDC2) return -1;
+		return puntDC1;
 	}
 }
