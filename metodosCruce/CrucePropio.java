f6436bce32dc45c8f161174e181a8decd68da03b
Andrea Martínez <deitamf@hotmail.com>
2019-04-01 02:38:50 +0200

    Metodos Propios funcionando
    Cruce, mutacion y seleccion propios funcionando.

--- metodosCruce/CrucePropio.java
+++ metodosCruce/CrucePropio.java
@@ -1,10 +1,66 @@
 package metodosCruce;
 
+import java.util.ArrayList;
+
 import base.Cromosoma;
 
+// copia uno si uno no los genes del padreX en el hijoX y el resto lo rellena con los genes del otro padre si no estan ya
 public class CrucePropio implements AlgoritmoCruce {
 
-	public void cruce(Cromosoma cromosoma, Cromosoma cromosoma2, Cromosoma hijo1, Cromosoma hijo2) {
+	public void cruce(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2) {
+//		ArrayList<Integer> genesP1 = new ArrayList<Integer>();
+//		ArrayList<Integer> genesP2 = new ArrayList<Integer>();
+		ArrayList<Integer> genesH1 = new ArrayList<Integer>();
+		ArrayList<Integer> genesH2 = new ArrayList<Integer>();
+		ArrayList<Integer> vaciosH1 = new ArrayList<Integer>();
+		ArrayList<Integer> vaciosH2 = new ArrayList<Integer>();
+		int nGenes = padre1.genes.length;
+
+		
+//		// inicializamos los ArrayList de los padres
+//		for(int i = 0; i < padre1.genes.length; i++) {
+//			genesP1.add(padre1.genes[i].getCiudad());
+//			genesP2.add(padre2.genes[i].getCiudad());
+//		}
+		
+		// copiamos genes alternos de padre1 en hijo1 y los de padre2 en hijo2
+		for (int i = 0; i < nGenes; i+=2) {
+			hijo1.genes[i].setCiudad(padre1.genes[i].getCiudad());
+			genesH1.add(padre1.genes[i].getCiudad());
+			hijo2.genes[i].setCiudad(padre2.genes[i].getCiudad());
+			genesH2.add(padre2.genes[i].getCiudad());
+		}
+		
+		// copiamos genes alternos (empezando por el segundo) de padre2 en hijo1 y los de padre1 en hijo2(si es posible)
+		for (int i = 1; i < nGenes; i+=2) {
+			if (!genesH1.contains(padre2.genes[i].getCiudad())) {
+				hijo1.genes[i].setCiudad(padre2.genes[i].getCiudad());
+				genesH1.add(padre2.genes[i].getCiudad());
+			} else vaciosH1.add(i);
+			if (!genesH2.contains(padre1.genes[i].getCiudad())) {
+				hijo2.genes[i].setCiudad(padre1.genes[i].getCiudad());
+				genesH2.add(padre1.genes[i].getCiudad());
+			} else vaciosH2.add(i);
+		}
+		
+		// Incluimos las ciudades que no hemos podido heredar del segundo padre
+		int indNoIncluido = 0;
+		while (!vaciosH1.isEmpty())
+			for (; indNoIncluido < nGenes; indNoIncluido++)
+				if (!genesH1.contains(indNoIncluido)) {
+					hijo1.genes[vaciosH1.get(0)].setCiudad(indNoIncluido);
+					vaciosH1.remove(0);
+					genesH1.add(indNoIncluido);
+				}
+		
+		indNoIncluido = 0;
+		while (!vaciosH2.isEmpty())
+			for (; indNoIncluido < nGenes; indNoIncluido++)
+				if (!genesH2.contains(indNoIncluido)) {
+					hijo1.genes[vaciosH2.get(0)].setCiudad(indNoIncluido);
+					vaciosH2.remove(0);
+					genesH2.add(indNoIncluido);
+				}
 		
 	}
 
--- metodosMutacion/MutacionPropia.java
+++ metodosMutacion/MutacionPropia.java
@@ -2,10 +2,39 @@ package metodosMutacion;
 
 import base.Cromosoma;
 
+// desplaza un bloque de genes una posicion a la derecha
 public class MutacionPropia implements AlgoritmoMutacion {
 
-	public void mutacion(Cromosoma[] pob, double probMutacion, int tampPob) {
-		
+	public void mutacion(Cromosoma[] pob, double probMutacion, int tamPob) {
+		boolean mutado;
+		double prob;
+		int genAux, nGenes = pob[0].getnGenes();
+		int puntDC1, puntDC2, puntDCAux;
+
+		for (int i = 0; i < tamPob; i++) {
+			mutado = false;
+
+			prob = Math.random();// se genera un numero aleatorio entre [0 1]
+			if (prob < probMutacion){// mutan los genes con prob<probMutacion
+				puntDC1 = (int) (Math.random()*nGenes);
+				puntDC2 = (int) (Math.random()*nGenes);
+				puntDCAux = puntDC1;
+				if (puntDC1 == puntDC2) puntDC2 = (puntDC2+1) % nGenes;
+				if (puntDC1 > puntDC2){
+					puntDC1 = puntDC2;
+					puntDC2 = puntDCAux;
+				}
+				
+				genAux = pob[i].genes[puntDC2].getCiudad();
+				for (int j = puntDC2; j > puntDC1; j--)
+					pob[i].genes[j].setCiudad(pob[i].genes[j-1].getCiudad());
+				pob[i].genes[puntDC1].setCiudad(genAux);
+				
+				mutado = true;
+			}
+			
+			if (mutado) pob[i].setFitness(pob[i].evaluaCromosoma());
+		}
 	}
 
 }
